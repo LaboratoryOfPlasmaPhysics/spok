321790e8517e44bdfd015ab6567e56ff2c9b5f1c
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2020-11-03 14:28:15 +0100

    remove old formisano (cartesian)

--- space/models/planetary.py
+++ space/models/planetary.py
@@ -8,7 +8,7 @@ from ..coordinates import coordinates as coords
 
 
 
-def checking_angles(theta, phi):
+def _checking_angles(theta,phi):
     theta = utils.listify(theta)
     phi = utils.listify(phi)
     if (len(np.shape(theta)) == 1) & (len(np.shape(phi)) == 1) & (len(theta) > 1) & (len(phi) > 1):
@@ -33,14 +33,13 @@ def _formisano1979(theta, phi, **kwargs):
 
 def formisano1979(theta, phi, **kwargs):
     '''
-    Formisano 2005 magnetopause model. Give the default position of the magnetopause.
+    Formisano 1979 magnetopause model. Give the default position of the magnetopause.
     function's arguments :
         - theta : angle in radiant, can be int, float or array (1D or 2D)
         - phi   : angle in radiant, can be int, float or array (1D or 2D)
     kwargs:
         - boundary  : "magnetopause", "bow_shock"
         - base  : can be "cartesian" (default) or "spherical"
-
 
     information : to get a particular point theta and phi must be an int or a float
     (ex : the nose of the boundary is given with the input theta=0 and phi=0). If a plan (2D) of
@@ -59,7 +58,7 @@ def formisano1979(theta, phi, **kwargs):
     else:
         raise ValueError("boundary: {} not allowed".format(kwargs["boundary"]))
 
-    theta, phi = checking_angles(theta, phi)
+    theta, phi = _checking_angles(theta, phi)
     r          =  _formisano1979(theta, phi, coefs = coefs)
     base       = kwargs.get("base", "cartesian")
     if base == "cartesian":
@@ -101,51 +100,6 @@ def Fairfield1971(x, args):
     pos=pd.DataFrame({'X' : np.concatenate([x, x[::-1]]),
                       'Y' : np.concatenate([yp, ym[::-1]]),
                       'Z' : np.concatenate([yp, ym[::-1]]),})
-
-    return pos.dropna()
-
-
-def Formisano1979(x, args):
-
-    '''
-    Formisano 1979 : Magnetopause and Bow shock models. Give positions of the boudaries in plans (XY) with Z=0 and (XZ) with Y=0.
-    function's arguments :
-        - x :  X axis (array) in Re (earth radii)
-        - args : coefficients Aij are determined from many boundary crossings and they depend on upstream conditions.
-
-        --> Default parameter for the bow shock and the magnetopause respectively are :
-            default_bs_formisano = [0.52,1,1.05,0.13,-0.16,-0.08,47.53,-0.42,0.67,-613]
-            default_mp_formisano = [0.65,1,1.16,0.03,-0.28,-0.11,21.41,0.46,-0.36,-221]
-
-     return : DataFrame (Pandas) with the position (X,Y,Z) in Re of the wanted boudary to plot (XY) and (XZ) plans.
-    '''
-
-
-    a11,a22,a33,a12,a13,a23,a14,a24,a34,a44 = args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9]
-
-    a_y = a22
-    b_y = a12*x + a24
-    c_y = a11*x**2 + a14*x + a44
-
-    delta_y =(b_y**2-4*a_y*c_y)
-
-
-    ym = (-b_y - np.sqrt(delta_y))/(2*a_y)
-    yp = (-b_y + np.sqrt(delta_y))/(2*a_y)
-
-    a_z = a33
-    b_z = a13*x + a34
-    c_z = a11*x**2 + a14*x + a44
-
-    delta_z =(b_z**2-4*a_z*c_z)
-
-    zm = (-b_z - np.sqrt(delta_z))/(2*a_z)
-    zp = (-b_z + np.sqrt(delta_z))/(2*a_z)
-
-
-    pos=pd.DataFrame({'X' : np.concatenate([x, x[::-1]]),
-                      'Y' : np.concatenate([yp, ym[::-1]]),
-                      'Z' : np.concatenate([zp, zm[::-1]]),})
 
     return pos.dropna()
 
---
+++ space/plot/planet_env.py
@@ -0,0 +1,59 @@
+
+import matplotlib.pyplot as plt
+
+def layout_EarthEnv_3planes(**kwargs):
+    #figsize=(15,4.5),xlim=(-30,30),ylim=(-30,30),zlim =(-30,30),alpha=0.5):
+
+    figsize = kwargs.get("kwargs", (15, 4.5))
+
+    fig, (ax0, ax1, ax2) = plt.subplots(ncols=3,figsize=figsize,constrained_layout=True)
+
+    ax0.set_xlabel('X (Re)')
+    ax0.set_ylabel('Y (Re)')
+    ax1.set_xlabel('X (Re)')
+    ax1.set_ylabel('Z (Re)')
+    ax2.set_xlabel('Y (Re)')
+    ax2.set_ylabel('Z (Re)')
+    ax0.axhline(0, color='k', ls='dotted', alpha=alpha)
+    ax0.axvline(0, color='k', ls='dotted', alpha=alpha)
+    ax1.axhline(0, color='k', ls='dotted', alpha=alpha)
+    ax1.axvline(0, color='k', ls='dotted', alpha=alpha)
+    ax2.axhline(0, color='k', ls='dotted', alpha=alpha)
+    ax2.axvline(0, color='k', ls='dotted', alpha=alpha)
+    ax0.set_xlim(xlim)
+    ax0.set_ylim(ylim)
+    ax1.set_xlim(xlim)
+    ax1.set_ylim(zlim)
+    ax2.set_xlim(ylim)
+    ax2.set_ylim(zlim)
+
+    return ax0,ax1,ax2
+
+# plot_boundaries(MP, BS, slice_x=22, slice_y=24, slice_z=0)
+
+
+def make_YZ_plan(pos):
+    a = np.linspace(0,2*np.pi,100)
+    r=abs(pos[(pos.X**2).argmin():  (pos.X**2).argmin()+1].Y.values)
+    return(r*np.cos(a),r*np.sin(a))
+
+
+
+
+
+def plot_boudaries(MP, BS, **kwargs):
+    style = kwargs.get("style", ['--k' , '--k'])
+    alpha = kwargs.get("alpha", 0.6)
+    axes = kwargs.get("axes", layout_EarthEnv_3planes(**kwargs))
+
+    if "slice_x" in kwargs:
+        axes[0].plot()
+
+    axes[0].plot(MP.X,MP.Y,style[0], alpha=alpha)
+    axes[0].plot(BS.X,BS.Y,style[1], alpha=alpha)
+
+    axes[1].plot(MP.X,MP.Z,style[0], alpha=alpha)
+    axes[1].plot(BS.X,BS.Z,style[1], alpha=alpha)
+
+    axes[2].plot(make_YZ_plan(MP)[0], make_YZ_plan(MP)[1],style[0],alpha=alpha)
+    axes[2].plot(make_YZ_plan(BS)[0], make_YZ_plan(BS)[1],style[1],alpha=alpha)
