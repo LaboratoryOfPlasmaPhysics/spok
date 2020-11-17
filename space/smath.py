6b640537325b0db8eeb300dc71616a27d48b5678
michotte <michotte@lx-michotte.lab-lpp.local>
2020-11-17 18:55:51 +0100

    fix bug resolve_poly2

--- space/smath.py
+++ space/smath.py
@@ -6,7 +6,7 @@ def norm(u, v, w):
 
 
 def resolve_poly2(a, b, c):
-    if isinstance(a, (np.ndarray | pd.Series):
+    if isinstance(a, np.ndarray )  | isinstance(a,pd.Series):
         r1 = np.zeros_like(a)
         r2 = np.zeros_like(a)
         a_null = np.where(np.abs(a) < 1e-6)[0]
@@ -15,9 +15,9 @@ def resolve_poly2(a, b, c):
         np.testing.assert_array_less(-delta, 0)
 
         r1, r2 = (-b + np.sqrt(delta)) / (2 * a), (-b + np.sqrt(delta)) / (2 * a)
-        if isinstance(c, (np.ndarray | pd.Series)):
+        if isinstance(c, np.ndarray )  | isinstance(c,pd.Series):
             c = c[a_null]
-        if isinstance(b, (np.ndarray | pd.Series)):
+        if isinstance(b, np.ndarray )  | isinstance(b,pd.Series):
             b = b[a_null]
         r1[a_null] = r2[a_null] = -c / b
     else:
